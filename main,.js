acafcc1dd0e38b83a73cd91cac0baf7f643f3f37
EduardoPR15 <lpeduardo15@gmail.com>
2022-09-03 14:57:04 -0500

    empezando

---
+++ main,.js
@@ -0,0 +1,9 @@
+const btnCart = document.querySelector(".bolsita")
+const btnheart = document.querySelector(".Corazon")
+console.log(btnheart); 
+btnheart.addEventListener("click", function(){
+    console.log(btnheart)})
+
+console.log(btnCart); 
+btnCart.addEventListener("click", function(){
+    console.log(btnCart)})
