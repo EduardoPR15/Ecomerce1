dd8a15b1762a86d9406c6ca3bd5eac136239ff78
EduardoPR15 <lpeduardo15@gmail.com>
2022-09-05 16:38:35 -0500

    ya casi

--- pruebas/shoppingCart/shoppingCart/js/showCart.js
+++
@@ -1,6 +0,0 @@
-const iconCart = document.querySelector("#icon_cart");
-const contentCart = document.querySelector("#contentCart");
-
-iconCart.addEventListener("click", () => {
-    contentCart.classList.toggle("content_cart-show");
-});
