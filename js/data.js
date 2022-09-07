2111fca27af4504059a8916f1bbe7db24d93c431
EduardoPR15 <lpeduardo15@gmail.com>
2022-09-06 20:01:59 -0500

    cambio de precios

--- js/data.js
+++ js/data.js
@@ -14,25 +14,25 @@ export const dataDB = [
         urlImg: "./Imagenes/id2.jpg"
     },
     {
-        id: 3 ,name: "Tres",stock: 6, precio: 50,  urlImg: "./Imagenes/id3.jpg"   
+        id: 3 ,name: "Tres",stock: 6, precio: 100,  urlImg: "./Imagenes/id3.jpg"   
     },
     {
         id: 4 ,name: "Cuatro",stock: 6, precio: 50,  urlImg: "./Imagenes/id4.jpg"   
     },
     {
-        id: 5 ,name: "Cinco",stock: 6, precio: 50,  urlImg: "./Imagenes/id5.jpg"   
+        id: 5 ,name: "Cinco",stock: 6, precio: 80,  urlImg: "./Imagenes/id5.jpg"   
     },
     {
         id: 6 ,name: "Seis",stock: 6, precio: 50,  urlImg: "./Imagenes/id6.jpg"   
     },
     {
-        id: 7 ,name: "Siete",stock: 6, precio: 50,  urlImg: "./Imagenes/id7.jpg"   
+        id: 7 ,name: "Siete",stock: 6, precio: 30,  urlImg: "./Imagenes/id7.jpg"   
     },
     {
         id: 8 ,name: "Ocho",stock: 6, precio: 50,   urlImg: "./Imagenes/id8.jpg"  
     },
     {
-        id: 9 ,name: "Nueve",stock: 6, precio: 50,  urlImg: "./Imagenes/id9.jpg"   
+        id: 9 ,name: "Nueve",stock: 6, precio: 35,  urlImg: "./Imagenes/id9.jpg"   
     },
     {
         id: 10 ,name:"Diez",stock: 6,  precio:50,  urlImg: "./Imagenes/id10.jpg"   
