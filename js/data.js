45d7959bfd78d7f6d9c1f07cdc8bd72c34f4fec6
EduardoPR15 <lpeduardo15@gmail.com>
2022-09-06 20:29:10 -0500

    precios

--- js/data.js
+++ js/data.js
@@ -1,40 +1,40 @@
 export const dataDB = [
     {
         id: 1,
-        name: "Uno",
+        name: "Escapulario Virgen del Carmen",
         stock: 6,
         precio: 50,
         urlImg: "./Imagenes/id1.jpg"
     },
     {
         id: 2,
-        name: "Dos",
+        name: "Escapulario San Miguel",
         stock: 6,
         precio: 50,
         urlImg: "./Imagenes/id2.jpg"
     },
     {
-        id: 3 ,name: "Tres",stock: 6, precio: 100,  urlImg: "./Imagenes/id3.jpg"   
+        id: 3 ,name: "Escapulario Virgen de Guadalupe",stock: 6, precio: 100,  urlImg: "./Imagenes/id3.jpg"   
     },
     {
-        id: 4 ,name: "Cuatro",stock: 6, precio: 50,  urlImg: "./Imagenes/id4.jpg"   
+        id: 4 ,name: "Escapulario Sagrado Corazon",stock: 6, precio: 50,  urlImg: "./Imagenes/id4.jpg"   
     },
     {
-        id: 5 ,name: "Cinco",stock: 6, precio: 80,  urlImg: "./Imagenes/id5.jpg"   
+        id: 5 ,name: "Mayoreo Escapulario",stock: 6, precio: 80,  urlImg: "./Imagenes/id5.jpg"   
     },
     {
-        id: 6 ,name: "Seis",stock: 6, precio: 50,  urlImg: "./Imagenes/id6.jpg"   
+        id: 6 ,name: "Escapulario San Miguel",stock: 6, precio: 50,  urlImg: "./Imagenes/id6.jpg"   
     },
     {
-        id: 7 ,name: "Siete",stock: 6, precio: 30,  urlImg: "./Imagenes/id7.jpg"   
+        id: 7 ,name: "Escapulario Virgen Guadalupe",stock: 6, precio: 30,  urlImg: "./Imagenes/id7.jpg"   
     },
     {
-        id: 8 ,name: "Ocho",stock: 6, precio: 50,   urlImg: "./Imagenes/id8.jpg"  
+        id: 8 ,name: "EScapulario Virgen del Carmen",stock: 6, precio: 50,   urlImg: "./Imagenes/id8.jpg"  
     },
     {
-        id: 9 ,name: "Nueve",stock: 6, precio: 35,  urlImg: "./Imagenes/id9.jpg"   
+        id: 9 ,name: "Cadena Acero Inoxidable",stock: 6, precio: 100,  urlImg: "./Imagenes/id9.jpg"   
     },
     {
-        id: 10 ,name:"Diez",stock: 6,  precio:50,  urlImg: "./Imagenes/id10.jpg"   
+        id: 10 ,name:"Argolla Mano",stock: 6,  precio:100,  urlImg: "./Imagenes/id10.jpg"   
     },
 ]
