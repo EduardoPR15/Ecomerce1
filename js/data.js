2cfe1bc985b2cec3861928b47134f06b041d4807
EduardoPR15 <lpeduardo15@gmail.com>
2022-10-14 14:20:48 -0500

    proyecto

--- js/data.js
+++ js/data.js
@@ -1,40 +1,40 @@
 export const dataDB = [
     {
         id: 1,
-        name: "Escapulario Virgen del Carmen",
+        name: "Camisa de pana con bolsillo",
         stock: 6,
-        precio: 50,
-        urlImg: "./Imagenes/id1.jpg"
+        precio: 449,
+        urlImg: "./Imagenes/Foto1.jpeg"
     },
     {
         id: 2,
-        name: "Escapulario San Miguel",
+        name: "Pantalones rectos unicolor",
         stock: 6,
-        precio: 50,
-        urlImg: "./Imagenes/id2.jpg"
+        precio: 425,
+        urlImg: "./Imagenes/Foto2.jpeg"
     },
     {
-        id: 3 ,name: "Escapulario Virgen de Guadalupe",stock: 6, precio: 100,  urlImg: "./Imagenes/id3.jpg"   
+        id: 3 ,name: "Camiseta tejida de canale",stock: 6, precio: 182,  urlImg: "./Imagenes/Foto3.jpeg"   
     },
     {
-        id: 4 ,name: "Escapulario Sagrado Corazon",stock: 6, precio: 50,  urlImg: "./Imagenes/id4.jpg"   
+        id: 4 ,name: "Camisa y short",stock: 6, precio: 509,  urlImg: "./Imagenes/Foto4.jpeg"   
     },
     {
-        id: 5 ,name: "Mayoreo Escapulario",stock: 6, precio: 80,  urlImg: "./Imagenes/id5.jpg"   
+        id: 5 ,name: "Abrigo en contraste Teddy bolsillo",stock: 6, precio: 701,  urlImg: "./Imagenes/Foto5.jpeg"   
     },
     {
-        id: 6 ,name: "Escapulario San Miguel",stock: 6, precio: 50,  urlImg: "./Imagenes/id6.jpg"   
+        id: 6 ,name: "Camisa unicolor DAZY",stock: 6, precio: 451,  urlImg: "./Imagenes/Foto6.jpeg"   
     },
     {
-        id: 7 ,name: "Escapulario Virgen Guadalupe",stock: 6, precio: 30,  urlImg: "./Imagenes/id7.jpg"   
+        id: 7 ,name: "Vestido de manfa con volante de cuello cuadrado",stock: 6, precio: 437,  urlImg: "./Imagenes/Foto7.jpeg"   
     },
     {
-        id: 8 ,name: "EScapulario Virgen del Carmen",stock: 6, precio: 50,   urlImg: "./Imagenes/id8.jpg"  
+        id: 8 ,name: "Short liso casual",stock: 6, precio: 180,   urlImg: "./Imagenes/Foto8.jpeg"  
     },
     {
-        id: 9 ,name: "Cadena Acero Inoxidable",stock: 6, precio: 100,  urlImg: "./Imagenes/id9.jpg"   
+        id: 9 ,name: "Vestido con cinturon ",stock: 6, precio: 386,  urlImg: "./Imagenes/Foto9.jpeg"   
     },
     {
-        id: 10 ,name:"Argolla Mano",stock: 6,  precio:100,  urlImg: "./Imagenes/id10.jpg"   
+        id: 10 ,name:"Abrigo con bolsillo",stock: 6,  precio:218,  urlImg: "./Imagenes/Foto10.jpeg"   
     },
 ]
